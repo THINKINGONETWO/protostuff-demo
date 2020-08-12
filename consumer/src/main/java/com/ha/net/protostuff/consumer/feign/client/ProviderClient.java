ae256381203a81c31de4d8b94b972c5846db9296
THINKINGONETWO <913019524@qq.com>
2020-08-12 16:47:22 +0800

    remove jackson-mapper-asl and jackson-core-asl dependency

--- consumer/src/main/java/com/ha/net/protostuff/consumer/feign/client/ProviderClient.java
+++ consumer/src/main/java/com/ha/net/protostuff/consumer/feign/client/ProviderClient.java
@@ -1,6 +1,5 @@
 package com.ha.net.protostuff.consumer.feign.client;
 import com.ha.net.common.data.User;
-//import com.ha.net.protostuff.consumer.config.CustomFeignClientConfiguration;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
